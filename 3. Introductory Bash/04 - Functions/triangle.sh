9e70919f477aabb101cfc9de957146f65cbf813c
joshackland <joshackland98@gmail.com>
2020-06-25 17:47:37 +0800

    Modified triangle.sh to accept arguments

--- 3. Introductory Bash/04 - Functions/triangle.sh
+++ 3. Introductory Bash/04 - Functions/triangle.sh
@@ -3,4 +3,10 @@ triangleArea()
     echo "The area for a triangle with base: $1 and height: $2 is: "$(( $1 * $2 / 2 ))
 }
 
-triangleArea 10 10
+for (( i=1; i<=5; i++ ))
+do
+    for (( j=1; j<=10; j++ ))
+    do
+        triangleArea $i $j
+    done
+done
